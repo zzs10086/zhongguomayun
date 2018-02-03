6bbd328c15cd592f6316cac8f0d3a829bb62f6e1
zzs10086 <563315963@qq.com>
2018-02-03 13:45:59 +0800

    modify

--- resources/views/layouts/m/main.blade.php
+++ resources/views/layouts/m/main.blade.php
@@ -47,5 +47,6 @@
         s.parentNode.insertBefore(bp, s);
     })();
 </script>
+<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1272853601'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s13.cnzz.com/stat.php%3Fid%3D1272853601' type='text/javascript'%3E%3C/script%3E"));</script>
 </body>
 </html>
