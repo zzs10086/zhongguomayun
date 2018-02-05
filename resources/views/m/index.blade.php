dbf445a495ef14c5dbba52e3f01131ecc34eed5d
zzs <563315963@qq.com>
2018-02-05 15:26:52 +0800

    modify

--- resources/views/m/index.blade.php
+++ resources/views/m/index.blade.php
@@ -1,32 +1,32 @@
 <?php use App\Lib\Util;?>
 @extends('layouts.m.main')
 @section('content')
-<!-- slide -->
-<div id="slideBox" class="slideBox">
-    <div class="bd">
-        <ul>
-            @foreach($foucsNews as $k=>$v)
-            <li>
-                <a class="pic" href="{{Util::createArcUrl($v['id'],$v['created_at'],$v['is_img'],'m')}}"><img src="{{config('app.upload_url') . $v['thumb']}}" alt="{{$v['title']}}"/></a>
-                <a class="tit" href="{{Util::createArcUrl($v['id'],$v['created_at'],$v['is_img'],'m')}}">{{$v['title']}}</a>
-            </li>
-            @endforeach
-        </ul>
-    </div>
-</div>
-<script type="text/javascript">
-  TouchSlide({
-        slideCell:"#slideBox",
-        titCell:".hd ul",
-        mainCell:".bd ul",
-        effect:"leftLoop",
-        autoPage:false,
-        autoPlay:true
-    });
-</script>
-<!-- end -->
-<!-- 热门文章 -->
 <div class="news inner">
+    <!-- slide -->
+    <div id="slideBox" class="slideBox">
+        <div class="bd">
+            <ul>
+                @foreach($foucsNews as $k=>$v)
+                <li>
+                    <a class="pic" href="{{Util::createArcUrl($v['id'],$v['created_at'],$v['is_img'],'m')}}"><img src="{{config('app.upload_url') . $v['thumb']}}" alt="{{$v['title']}}"/></a>
+                    <a class="tit" href="{{Util::createArcUrl($v['id'],$v['created_at'],$v['is_img'],'m')}}">{{$v['title']}}</a>
+                </li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+    <script type="text/javascript">
+      TouchSlide({
+            slideCell:"#slideBox",
+            titCell:".hd ul",
+            mainCell:".bd ul",
+            effect:"leftLoop",
+            autoPage:false,
+            autoPlay:true
+        });
+    </script>
+    <!-- end -->
+    <!-- 热门文章 -->
     <div id="result">
 
 
