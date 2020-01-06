aa91f0fb198383d1d0561f4157ef65e89bacff9d
zzs <3004788270@qq.com>
2020-01-06 10:44:58 +0800

    modify wechat

--- app/Http/Controllers/Pc/WechatController.php
+++ app/Http/Controllers/Pc/WechatController.php
@@ -10,7 +10,25 @@ class WechatController extends Controller
 {
     //
      private $token = 'zgmy';
-     public function check_token(Request $request){
+
+     /**
+      * 处理微信的请求消息
+      *
+      * @return string
+      */
+     public function check_token()
+     {
+          Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+
+          $app = app('wechat.official_account');
+          $app->server->push(function($message){
+               return "欢迎关注 overtrue！";
+          });
+
+          return $app->server->serve();
+     }
+
+     public function check_tokens(Request $request){
 
          /* $signature = $request->get('signature');
           $nonce = $request->get('nonce');
